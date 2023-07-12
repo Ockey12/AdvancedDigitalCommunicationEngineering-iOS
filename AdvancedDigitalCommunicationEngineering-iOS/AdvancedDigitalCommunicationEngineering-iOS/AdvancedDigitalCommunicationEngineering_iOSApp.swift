a2ba456ea3b44fdd769a8de969e46009ef1ae471
OnagaHaruki <haloupgarou@gmail.com>
2023-07-13 01:13:26 +0900

    add: プロジェクトを作成した

---
+++ AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedDigitalCommunicationEngineering_iOSApp.swift
+//  AdvancedDigitalCommunicationEngineering-iOS
+//
+//  Created by オナガ・ハルキ on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedDigitalCommunicationEngineering_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AdvancedDigitalCommunicationEngineering-iOS
+//
+//  Created by オナガ・ハルキ on 2023/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
