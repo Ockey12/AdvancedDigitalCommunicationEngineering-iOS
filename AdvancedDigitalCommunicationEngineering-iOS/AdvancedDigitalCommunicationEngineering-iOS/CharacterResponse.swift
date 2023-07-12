37a3a6bedb64918c103ad1676c0d781ffa4854ca
OnagaHaruki <haloupgarou@gmail.com>
2023-07-13 01:31:23 +0900

    add: CharacterResponse構造体を追加した

---
+++ AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering-iOS/CharacterResponse.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct CharacterResponse {
+    let name: String
+    let school: String
+    let grade: String
+    let role: String
+    let voice: String
+    let comment: String
+    let imageURL: URL
+}
