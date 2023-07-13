1f39a47edede2210614604fd26f92dab7a90b746
OnagaHaruki <haloupgarou@gmail.com>
2023-07-13 21:06:45 +0900

    add: CharacterInfoView構造体を追加した

--- AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering-iOS/Model/CharacterInfo.swift
+++ AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering-iOS/Model/CharacterInfo.swift
@@ -7,5 +7,5 @@ struct CharacterInfo: Codable {
     var role = ""
     var voice = ""
     var comment = ""
-    var imageURL = URL(string: "")
+    var imageURL = URL(string: "http://133.54.230.183:8000/static/guiter_hero.png")!
 }
---
+++ AdvancedDigitalCommunicationEngineering-iOS/AdvancedDigitalCommunicationEngineering-iOS/View/CharacterInfoView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct CharacterInfoView: View {
+    var characterInfo: CharacterInfo
+    var body: some View {
+        VStack {
+            Text(characterInfo.name)
+            Text(characterInfo.school)
+            Text(characterInfo.grade)
+            Text(characterInfo.role)
+            Text(characterInfo.voice)
+            Text(characterInfo.comment)
+        }
+    }
+}
+
+struct CharacterInfoView_Previews: PreviewProvider {
+    static var characterInfo = CharacterInfo(name: "後藤 ひとり",
+                                             school: "秀華高校",
+                                             grade: "1年生",
+                                             role: "リードギター・作詞",
+                                             voice: "青山吉能",
+                                             comment: "絶対いやだ！働きたくない！！怖い！社会が怖い！！",
+                                             imageURL: URL(string: "http://\(PlistAccessor.serverIpAddress):8000/static/guiter_hero.png")!
+    )
+    static var previews: some View {
+        CharacterInfoView(characterInfo: characterInfo)
+    }
+}
